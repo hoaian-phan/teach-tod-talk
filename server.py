f305f1e9462f5029a85a9e51d95acc0979cd7379
Hoai-An Phan <pnhoaian2904@gmail.com>
2022-07-29 12:07:29 -0700

    Added helper function to general list of words for each lesson plan

--- server.py
+++ server.py
@@ -8,11 +8,27 @@ app = Flask(__name__)
 app.secret_key = os.environ['SECRET_KEY']
 
 # hard-coded for now, add database later
-COLORS = {
-  "easy": ['black', 'blue', 'brown', 'green', 'orange', 'pink', 'purple', 'red', 'white', 'yellow'],
-  "medium": ['grey', 'silver', 'gold', 'navy', 'chocolate', 'tan', 'aqua', 'tomato', 'violet', 'teal'],
-  "advanced": ['olive', 'maroon', 'snow', 'magenta', 'salmon', 'coral', 'lime', 'orchid', 'khaki', 'cyan', 'plum', 'turquoise', 'beige', 'indigo'],
+CATEGORIES = {
+  "colors": { "easy": ['black', 'blue', 'brown', 'green', 'orange', 'pink', 'purple', 'red', 'white', 'yellow'],
+              "medium": ['grey', 'silver', 'gold', 'navy', 'chocolate', 'tan', 'aqua', 'tomato', 'violet', 'teal'],
+              "advanced": ['olive', 'maroon', 'snow', 'magenta', 'salmon', 'coral', 'lime', 'orchid', 'khaki', 'cyan', 'plum', 'turquoise', 'beige', 'indigo'],
+            },
+  "animals": {"easy": ['dog', 'cat', 'cow', 'chicken', 'duck', 'sheep', 'pig', 'fish', 'bird', 'horse'],
+              "medium": ['lion', 'brea', 'tiger', 'goat', 'monkey', 'deer', 'mouse', 'dolphin', 'crab', 'frog'],
+              "advanced": ['zebra', 'shark', 'rabbit', 'shrimp', 'bee', 'butterfly', 'lobster', 'caterpillar', 'octopus', 'elephant', 'pigeon', 'donkey', 'turkey', 'squirrel', 'panda', 'giraffe']
+            },
 }
+# COLORS = {
+#   "easy": ['black', 'blue', 'brown', 'green', 'orange', 'pink', 'purple', 'red', 'white', 'yellow'],
+#   "medium": ['grey', 'silver', 'gold', 'navy', 'chocolate', 'tan', 'aqua', 'tomato', 'violet', 'teal'],
+#   "advanced": ['olive', 'maroon', 'snow', 'magenta', 'salmon', 'coral', 'lime', 'orchid', 'khaki', 'cyan', 'plum', 'turquoise', 'beige', 'indigo'],
+# }
+
+# ANIMALS = {
+#   "easy": ['dog', 'cat', 'cow', 'chicken', 'duck', 'sheep', 'pig', 'fish', 'bird', 'horse'],
+#   "medium": ['lion', 'brea', 'tiger', 'goat', 'monkey', 'deer', 'mouse', 'dolphin', 'crab', 'frog'],
+#   "advanced": ['zebra', 'shark', 'rabbit', 'shrimp', 'bee', 'butterfly', 'lobster', 'caterpillar', 'octopus', 'elephant', 'pigeon', 'donkey', 'turkey', 'squirrel', 'panda', 'giraffe']
+# }
 
 @app.route("/")
 def home():
@@ -20,6 +36,23 @@ def home():
 
   return render_template("home.html")
 
+def generate_list(category, num_words, level):
+  lst = []
+  if level == 'easy':
+    lst = sample(CATEGORIES[category]['easy'], num_words)
+  elif level == 'medium':
+    easy_num = num_words // 2
+    medium_num = num_words - easy_num
+    lst = sample(CATEGORIES[category]['easy'], easy_num)
+    lst.extend(sample(CATEGORIES[category]['medium'], medium_num))
+  else:
+    easy_num = medium_num = num_words // 3
+    advanced_num = num_words - (easy_num + medium_num)
+    lst = sample(CATEGORIES[category]['easy'], easy_num)
+    lst.extend(sample(CATEGORIES[category]['medium'], medium_num))
+    lst.extend(sample(CATEGORIES[category]['advanced'], advanced_num))
+  print(lst)
+  return lst
 
 @app.route("/build_lesson")
 def customize():
@@ -29,28 +62,21 @@ def customize():
   level = request.args.get("level")
 
   # Colors:
-  color_list = []
   if category == 'colors':
-    if level == 'easy':
-      color_list = sample(COLORS['easy'], num_words)
-    elif level == 'medium':
-      easy_num = num_words // 2
-      medium_num = num_words - easy_num
-      color_list = sample(COLORS['easy'], easy_num)
-      color_list.extend(sample(COLORS['medium'], medium_num))
-    else:
-      easy_num = medium_num = num_words // 3
-      advanced_num = num_words - (easy_num + medium_num)
-      color_list = sample(COLORS['easy'], easy_num)
-      color_list.extend(sample(COLORS['medium'], medium_num))
-      color_list.extend(sample(COLORS['advanced'], advanced_num))
-    print(color_list)
+    color_list = generate_list(category=category, num_words=num_words, level=level)
 
     # Return jsonify if the request is for json, else render html template
     if request.headers['Accept'] == 'application/json':
       return jsonify({"status": "success", "colors": color_list})
-  
+
     return render_template("colors.html", colors=color_list)
+
+  # Animals
+  elif category == 'animals':
+    animal_list = generate_list(category=category, num_words=num_words, level=level)
+
+    return render_template("animals.html", animals=animal_list)
+
   
 
 
